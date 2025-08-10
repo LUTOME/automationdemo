afab4404ac98a8cc73150080b486c05bbcc724f9
Lutome <lucas.t.medina1994@gmail.com>
2025-08-10 15:09:56 -0700

    100% set

---
+++ cypress/e2e/Tests/Checkout/GX3-1870_spaceCheckout.cy.ts
@@ -0,0 +1,51 @@
+import { parseCurrencyNum } from '@helper/testUtilities';
+
+describe('GX3-1870: SpaceBeyond | Booking | Book a Destination in Checkout', () => {
+	beforeEach(() => {
+		//todo: hacer Login
+		cy.visit('https://demo.testim.io');
+		cy.contains('Log in').click();
+
+		cy.page().then(({ spaceLoginPage }) => {
+			spaceLoginPage.enterUsername('Admin');
+			spaceLoginPage.enterPassword('admin123');
+			spaceLoginPage.submitLogin();
+		});
+	});
+
+	it('GX3-1870 | TC1: Should book a destination when checkout form is filled', function () {
+		cy.page().then(({ spaceProductPage, spaceCheckoutPage }) => {
+			//* ESTRATEGIAS DE LOCALIZADOR por "GIVEN DATA" hay 2 opciones:
+
+			//* Option 1: Estrategia "DATA GENERADA"
+			// Cuando la data es generada por nuestras consecuencias (ejemplo: crear un producto con nombre 'Madan')
+			spaceProductPage.getCardByTitle('Madan');
+			// También puede ser por índice:
+			spaceProductPage.getCardByIndex(0);
+
+			//* Option 2: Estrategia "DATA MUESTRA" o (DATA RANDOM)
+			// Cuando la data no es generada por nuestras consecuencias sino está ahí (y tiene propencia al cambio)
+			// Seleccionando todas las opciones y eligiendo una al azar:
+			spaceProductPage.getAnyCard().then(givenCard => {
+				//todo: Obtener las propiedades del item seleccionado e interactuar con él:
+				spaceProductPage.getCardData(givenCard).then(cardData => {
+					cy.wrap(cardData.title).as('cardTitle');
+					cy.wrap(cardData.description).as('cardDesc');
+					cy.wrap(cardData.price).as('cardPrice');
+					cy.wrap(cardData.bookButton).click();
+				});
+			});
+			cy.url().should('include', '/checkout');
+
+			spaceCheckoutPage.checkoutPrice().invoke('text').as('checkoutPice');
+			cy.then(() => {
+				cy.log(this.cardTitle as string);
+				cy.log(this.cardDesc as string);
+				const expectedCardPrice = parseCurrencyNum(this.cardPrice as string);
+				const checkoutPiceValue = parseCurrencyNum(this.checkoutPice as string);
+
+				expect(checkoutPiceValue).equal(expectedCardPrice);
+			});
+		});
+	});
+});
